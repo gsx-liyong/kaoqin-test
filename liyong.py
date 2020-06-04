ec3bf93adf9c14d5a67f6f81b03f5101928cbd3e
unknown <liyong@baijiahulian.com>
2020-06-04 15:09:52 +0800

    用pycharm提交的

---
+++ liyong.py
@@ -0,0 +1,16 @@
+#！user/bin/env python
+# coding=utf-8
+
+'''
+@File  : liyong.py
+@Author: 李 永
+@Date  : 2020/6/4 14:46
+@Desc  : 
+'''
+
+###检查字符串是否ip
+def is_ip(ip):
+	num_list=ip.split(".")
+	if len(num_list)!=4:
+		return False
+	for num in  num_list:
